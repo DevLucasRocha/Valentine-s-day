d4b24a6f86583549ab498677ba6caa1d37ed04bb
Lucas Rocha <lucas_santos239@outlook.com>
2025-06-09 20:58:43 -0300

    Update LoveButton.jsx

--- valentine-days/src/components/LoveButton.jsx
+++ valentine-days/src/components/LoveButton.jsx
@@ -7,9 +7,6 @@ const LoveButton = forwardRef(({ tipo, children, onMouseOver, onClick }, ref) =>
     ref: tipo === 'nao' ? ref : null,
     onMouseOver: tipo === 'nao' ? onMouseOver : undefined,
     onClick,
-    style: tipo === 'nao'
-      ? { position: 'absolute', left: '65%', top: '10%' }
-      : { position: 'relative', left: '0', top: '0' },
     type: 'button'
   };
 
